eb6524519f85a670e2e7131eea4481fd3c33493b
Sunny <Sunnylaw3571@gmail.com>
2024-05-16 11:26:45 +0100

    Create Rover with the ability to set up the initial position

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+        Rover test = new Rover(-1,2, Rover.Point.S);
+        System.out.println(test);
+    }
 }
--- src/main/java/Plateau.java
+++ src/main/java/Plateau.java
@@ -1,2 +1,17 @@
 public class Plateau {
+    int gridX;
+    int gridY;
+    Rover rover;
+
+    public Plateau(int gridX, int gridY, Rover rover) {
+        this.gridX = gridX;
+        this.gridY = gridY;
+        this.rover = rover;
+
+        if(rover.positionX>gridX || rover.positionY>gridY || rover.positionX<0 || rover.positionY<0){
+            System.out.println("Invalid input");;
+        } else {
+
+        }
+    }
 }
