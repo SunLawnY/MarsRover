5f930380bbeb3cad395f34d8148ff9b9ebe15cb3
SunLawnY <sunnylaw3571@gmail.com>
2024-06-10 21:27:24 +0100

    test

--- src/test/java/InputAndParser/AdminRemoteTest.java
+++ src/test/java/InputAndParser/AdminRemoteTest.java
@@ -2,19 +2,19 @@ package InputAndParser;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AdminRemoteTest {
 
     @Test
     void createRover() {
-//        AdminRemote test = new AdminRemote()
-
     }
 
     @Test
-    void performMovement() {
+    void storeTheInstruction() {
+    }
+
+    @Test
+    void startAllRovers() {
     }
 }
